928cb4cb21b20135f91cbd7d8222bd6ecd79b4bf
Prateek Kumar <89418989+prateekkumaroriginal@users.noreply.github.com>
2023-06-17 17:54:39 +0530

    Update index.js

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,8 +1,57 @@
-keys = document.querySelectorAll(".btn");
+buttons = document.querySelectorAll(".btn")
 
-keys.forEach(elem => {
-    elem.addEventListener("click", () => {
-        alert("I got clicked")
+const makeSound = (key) => {
+    switch (key) {
+        case "w":
+            audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        case "k":
+            audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "l":
+            audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
+
+buttons.forEach(elem => {
+    elem.addEventListener("click", (e)=>{
+        let btn_key = elem.innerHTML
+        makeSound(btn_key)
+        buttonAnimation(btn_key)
     })
 });
 
+document.addEventListener("keydown", (event)=>{
+    makeSound(event.key)
+    buttonAnimation(event.key)
+})
+
+const buttonAnimation = (key)=>{
+    let activeButton = document.querySelector("."+key)
+    activeButton.classList.add("pressed")
+    setTimeout(()=>{
+        activeButton.classList.remove("pressed")
+    }, 100)
+}
